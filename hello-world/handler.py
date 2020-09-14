bafd3d2c8269b0232dcdb6929fc9a5d1943db1fd
nafisur <nafisur.rahman21@gmail.com>
2020-09-14 16:22:46 +0530

    hello world

--- hello-world/handler.py
+++ hello-world/handler.py
@@ -6,12 +6,15 @@ def hello(event, context):
         "message": "Go Serverless v1.0! Your hello-world function executed successfully!",
         "input": event
     }
-
     print(body['message'])
-
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
 
     return response
+
+
+if __name__ == '__main__':
+    res = hello(event={"name": "Nafisur", "title": "Rahman"}, context='None')
+    print(res)
